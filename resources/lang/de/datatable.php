bb73622f1e120bba6561012cb06dc54e09825a09
Daniel Sys <79267265+dansysanalyst@users.noreply.github.com>
2021-04-24 16:24:15 +0200

    de i18n

---
+++ resources/lang/de/datatable.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+    'buttons' => [
+        'filter' => 'Aktionen',
+        'export' => 'Exportieren',
+        'export_one' => '"Auswahl exportieren"',
+        'export_selected' => 'Auswahlen exportieren'
+    ],
+    'labels' => [
+        'action' => 'Aktionen',
+        'results_per_page' => 'Ergebnisse pro Seite',
+        'clear_filter' => 'Filter löschen',
+        'no_data' => 'Keine Ergebnisse gefunden'
+    ],
+    'placeholders' => [
+        'search' => 'Suche',
+        'select' => 'wählen Sie einen Zeitraum',
+        'multi_select' => 'Bitte wählen'
+    ],
+    'pagination' => [
+        'showing' => 'Anzeigen von',
+        'to' => 'bis',
+        'of' => 'von',
+        'results' => 'Ergebnissen'
+    ]
+];
