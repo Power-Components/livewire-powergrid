157904b089fbfecc4d82715fb0c1083f46ade855
Luan Freitas <luanfreitasgoncalves@gmail.com>
2021-08-11 08:37:40 -0300

    Add Farsi/Persian Language

---
+++ resources/lang/fa/datatable.php
@@ -0,0 +1,47 @@
+<?php
+
+return [
+    'buttons' => [
+        'filter'          => 'فیلتر',
+        'export'          => 'ExportXLS',
+        'export_one'      => 'ExportXLS انتخاب شده',
+        'export_selected' => 'ExportXLS انتخاب شده'
+    ],
+    'labels' => [
+        'action'           => 'اقدام ها',
+        'results_per_page' => 'آیتم در صفحه',
+        'clear_filter'     => 'بدون فیلتر',
+        'no_data'          => 'هیچ رکوردی یافت نشد',
+        'all'              => 'همه'
+    ],
+    'placeholders' => [
+        'search' => 'جستجو...',
+        'select' => 'انتخاب یک بازه زمانی',
+    ],
+    'pagination' => [
+        'showing' => 'نمایش',
+        'to'      => 'تا',
+        'of'      => 'از',
+        'results' => 'نتایج',
+        'all'     => 'همه'
+    ],
+    'multi_select' => [
+        'select' => 'انتخاب',
+        'all'    => 'همه'
+    ],
+    'select' => [
+        'select' => 'انتخاب',
+        'all'    => 'همه'
+    ],
+    'boolean_filter' => [
+        'all' => 'همه'
+    ],
+    'input_text_options' => [
+        'is'           => 'برابرباشد',
+        'is_not'       => 'برابرنباشد',
+        'contains'     => 'شامل',
+        'contains_not' => 'شامل نباشد',
+        'starts_with'  => 'شروع شود با',
+        'ends_with'    => 'خاتمه یابد با',
+    ]
+];
