6355062975c20bd2990c9184813b004b02fdf425
Luca Longo <masterix21@gmail.com>
2021-09-23 10:05:00 +0200

    IT translations

---
+++ resources/lang/it/datatable.php
@@ -0,0 +1,45 @@
+<?php
+
+return [
+    'buttons' => [
+        'filter'          => 'Filtra',
+    ],
+    'labels' => [
+        'action'           => 'Azioni',
+        'results_per_page' => 'Risultati per pagina',
+        'clear_filter'     => 'Rimuovi filtri',
+        'no_data'          => 'Nessun risultato',
+        'all'              => 'Tutti',
+        'selected'         => 'Selezionati',
+    ],
+    'placeholders' => [
+        'search' => 'Cerca...',
+        'select' => 'Scegli un periodo',
+    ],
+    'pagination' => [
+        'showing' => 'Visualizzi',
+        'to'      => 'a',
+        'of'      => 'di',
+        'results' => 'Risultati',
+        'all'     => 'Tutti',
+    ],
+    'multi_select' => [
+        'select' => 'Seleziona',
+        'all'    => 'Tutti',
+    ],
+    'select' => [
+        'select' => 'Seleziona',
+        'all'    => 'Tutti'
+    ],
+    'boolean_filter' => [
+        'all' => 'Tutti',
+    ],
+    'input_text_options' => [
+        'is'           => 'È',
+        'is_not'       => 'Non è',
+        'contains'     => 'Contiene',
+        'contains_not' => 'Non contiene',
+        'starts_with'  => 'Inizia con',
+        'ends_with'    => 'Termina con',
+    ]
+];
