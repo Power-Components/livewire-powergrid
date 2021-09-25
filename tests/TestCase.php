3da64433f8534834278e4660cb1562a0a9c2cdf9
Daniel Ang <daniel@sysanalyst.eu>
2021-09-25 23:20:16 +0200

    Fix testCase
    Fix "Target class [livewire] does not exist."

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,12 +3,14 @@
 namespace PowerComponents\LivewirePowerGrid\Tests;
 
 use PowerComponents\LivewirePowerGrid\Providers\PowerGridServiceProvider;
+use Livewire\LivewireServiceProvider;
 
 class TestCase extends \Orchestra\Testbench\TestCase
 {
     protected function getPackageProviders($app)
     {
         return [
+            LivewireServiceProvider::class,
             PowerGridServiceProvider::class
         ];
     }
