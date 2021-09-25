885b3cc83aaaf52fe234cc6d81c4266ab11699f5
Daniel Ang <daniel@sysanalyst.eu>
2021-09-25 23:27:58 +0200

    use BaseTestCase
    Improve readability

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,8 +4,9 @@ namespace PowerComponents\LivewirePowerGrid\Tests;
 
 use PowerComponents\LivewirePowerGrid\Providers\PowerGridServiceProvider;
 use Livewire\LivewireServiceProvider;
+use \Orchestra\Testbench\TestCase as BaseTestCase;
 
-class TestCase extends \Orchestra\Testbench\TestCase
+class TestCase extends BaseTestCase
 {
     protected function getPackageProviders($app)
     {
