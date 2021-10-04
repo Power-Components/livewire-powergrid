017db0156dc81c5f6c7625f977e3a72b1ac4fb37
Daniel Ang <daniel@sysanalyst.eu>
2021-10-04 11:34:12 +0200

    Added filter with formatted currency

--- tests/Feature/FilterNumberTest.php
+++ tests/Feature/FilterNumberTest.php
@@ -4,7 +4,7 @@ use PowerComponents\LivewirePowerGrid\Tests\DishesTable;
 
 it('properly filters by "min"')
     ->livewire(DishesTable::class)
-    ->set('filters', filterNumber('id', min: 2, max: null))
+    ->set('filters', filterNumber('id', min: '2', max: null, thousands: '', decimal: ''))
     ->assertSeeHtml('Peixada da chef Nábia')
     ->assertSeeHtml('Francesinha')
     ->assertSeeHtml('борщ')
@@ -12,7 +12,7 @@ it('properly filters by "min"')
    
 it('properly filters by "max"')
     ->livewire(DishesTable::class)
-    ->set('filters', filterNumber('id', min: null, max: 3))
+    ->set('filters', filterNumber('id', min: null, max: '3', thousands: '', decimal: ''))
     ->assertSeeHtml('Pastel de Nata')
     ->assertSeeHtml('Peixada da chef Nábia')
     ->assertSeeHtml('Carne Louca')
@@ -20,14 +20,14 @@ it('properly filters by "max"')
 
 it('properly filters by "min & max"')
     ->livewire(DishesTable::class)
-    ->set('filters', filterNumber('id', min: 1, max: 2))
+    ->set('filters', filterNumber('id', min: '1', max: '2', thousands: '', decimal: ''))
     ->assertSeeHtml('Pastel de Nata')
     ->assertSeeHtml('Peixada da chef Nábia')
     ->assertDontSeeHtml('Carne Louca');
 
 it('properly filters by "min & max" currency')
     ->livewire(DishesTable::class)
-    ->set('filters', filterNumber('price', min: 60.49, max: 100))
+    ->set('filters', filterNumber('price', min: '60.49', max: '100', thousands: '', decimal: ''))
     ->assertSeeHtml('Francesinha')
     ->assertSeeHtml('Barco-Sushi da Sueli')
     ->assertSeeHtml('Barco-Sushi Simples')
@@ -36,28 +36,34 @@ it('properly filters by "min & max" currency')
 
 it('ignores null "min & max"')
     ->livewire(DishesTable::class)
-    ->set('filters', filterNumber('id', min: null, max: null))
+    ->set('filters', filterNumber('id', min: null, max: null, thousands: '', decimal: ''))
     ->assertSeeHtml('Pastel de Nata')
     ->assertSeeHtml('Peixada da chef Nábia')
     ->assertSeeHtml('борщ');
 
 it('displays "No records found" with non-existent min')
     ->livewire(DishesTable::class)
-    ->set('filters', filterNumber('id', min: 1000000, max: null))
+    ->set('filters', filterNumber('id', min: '1000000', max: null, thousands: '', decimal: ''))
     ->assertSeeHtml('No records found')
     ->assertDontSeeHtml('Pastel de Nata');
 
-
+it('properly filters by "min & max" formatted')
+    ->livewire(DishesTable::class)
+    ->set('filters', filterNumber('price', min: '1,50', max: '20,51', thousands: '.', decimal: ','))
+    ->assertSeeHtml('Pastel de Nata')
+    ->assertSeeHtml('Peixada da chef Nábia')
+    ->assertDontSeeHtml('Carne Louca');
+    
 //Helper
-function filterNumber(string $field, ?int $min, ?int $max): array
+function filterNumber(string $field, ?string $min, ?string $max, ?string $thousands, ?string $decimal): array
 {
     return [
         'number' => [
             $field =>  [
                 'start' => $min,
                 'end' => $max,
-                'thousands' => '',
-                'decimal' => ''
+                'thousands' => $thousands,
+                'decimal' => $decimal
             ]
         ]
     ];
