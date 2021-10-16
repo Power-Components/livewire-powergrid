e06af5fd55cb0fda1c4c32a8efe452395890c53b
Muhammad Adnan <naravaya04@gmail.com>
2021-10-16 19:49:11 +0700

    Add indonesian lang

---
+++ resources/lang/id/datatable.php
@@ -0,0 +1,45 @@
+<?php
+
+return [
+    'buttons' => [
+        'filter'          => 'Filter',
+    ],
+    'labels' => [
+        'action'           => 'Aksi',
+        'results_per_page' => 'Data per halaman',
+        'clear_filter'     => 'Hapus filter',
+        'no_data'          => 'Tidak ada data yang ditemukan',
+        'all'              => 'Semua',
+        'selected'         => 'Dipilih',
+    ],
+    'placeholders' => [
+        'search' => 'Cari...',
+        'select' => 'Pilih periode',
+    ],
+    'pagination' => [
+        'showing' => 'Menampilkan',
+        'to'      => 'kepada',
+        'of'      => 'dari',
+        'results' => 'Hasil',
+        'all'     => 'Semua',
+    ],
+    'multi_select' => [
+        'select' => 'Pilih',
+        'all'    => 'Semua',
+    ],
+    'select' => [
+        'select' => 'Pilih',
+        'all'    => 'Semua',
+    ],
+    'boolean_filter' => [
+        'all' => 'Semua',
+    ],
+    'input_text_options' => [
+        'is'           => 'Tepat',
+        'is_not'       => 'Tidak tepat',
+        'contains'     => 'Terkait',
+        'contains_not' => 'Tidak terkait',
+        'starts_with'  => 'Dimulai dengan',
+        'ends_with'    => 'Diakhiri dengan',
+    ]
+];
