8262a62321d16ff66f699d88672956b878275f75
Anton V. Davydov <james.rus52@gmail.com>
2022-02-13 18:37:11 +0300

    Add russian translation

---
+++ resources/lang/ru/datatable.php
@@ -0,0 +1,56 @@
+<?php
+
+return [
+    'buttons' => [
+        'filter' => 'Фильтр',
+    ],
+    'labels' => [
+        'action'           => 'Действия',
+        'results_per_page' => 'Записей на страницу',
+        'clear_filter'     => 'Удалить фильтры',
+        'no_data'          => 'Записи не найдены',
+        'all'              => 'Все',
+        'selected'         => 'Выбранные',
+    ],
+    'placeholders' => [
+        'search' => 'Поиск...',
+        'select' => 'Выбрать период',
+    ],
+    'pagination' => [
+        'showing' => 'Отображение',
+        'to'      => 'к',
+        'of'      => 'из',
+        'results' => 'Результаты',
+        'all'     => 'Все',
+    ],
+    'multi_select' => [
+        'select' => 'Выбрать',
+        'all'    => 'Все',
+    ],
+    'select' => [
+        'select' => 'Выбрать',
+        'all'    => 'Все',
+    ],
+    'boolean_filter' => [
+        'all' => 'Все',
+    ],
+    'input_text_options' => [
+        'is'           => 'Равно',
+        'is_not'       => 'Не равно',
+        'contains'     => 'Содержит',
+        'contains_not' => 'Не содержит',
+        'starts_with'  => 'Начинается с',
+        'ends_with'    => 'Заканчивается на',
+        'is_empty'     => 'Пусто',
+        'is_not_empty' => 'Не пусто',
+        'is_null'      => 'Пусто (null)',
+        'is_not_null'  => 'Не пусто (null)',
+        'is_blank'     => 'Пусто (blank)',
+        'is_not_blank' => 'Не пусто (blank)',
+
+    ],
+    'export' => [
+        'exporting' => 'Пожалуйста подождите!',
+        'completed' => 'Экспорт завершен! Файлы готово для скачивания',
+    ],
+];
