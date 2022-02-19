1ea22713c9de864445865d784500bb0ec331b55b
Stridesdata <82326559+Stridesdata@users.noreply.github.com>
2022-02-19 04:46:49 +0100

    Fix Pagination responsiveness on mobile

--- resources/views/components/frameworks/bootstrap5/footer.blade.php
+++ resources/views/components/frameworks/bootstrap5/footer.blade.php
@@ -1,8 +1,8 @@
 @if(!is_array($data))
-    <footer class="mt-50 pb-1 w-100 align-items-end px-1 d-flex flex-wrap justify-content-center justify-content-md-between">
-        <div class="col-auto overflow-auto mt-1 mt-sm-0 bs5-per-page">
+    <footer class="mt-50 pb-1 w-100 align-items-end px-1 d-flex flex-wrap">
+        <div class="col-md-3 col-12 me-auto">
             @if($perPageInput)
-                <div class="d-flex flex-column flex-lg-row align-items-center">
+                <div class="d-flex justify-content-center align-items-center">
                     <label class="w-auto">
                         <select wire:model="perPage" class="form-select">
                             @foreach($perPageValues as $value)
@@ -15,23 +15,18 @@
                             @endforeach
                         </select>
                     </label>
-                    <small class="ms-2 text-muted">
+                    <small class="ms-2">
                         {{ trans('livewire-powergrid::datatable.labels.results_per_page') }}
                     </small>
                 </div>
             @endif
         </div>
-        <div class="col-auto overflow-auto mt-1 mt-sm-0">
+        <div class="col-md-9 col-12 overflow-auto mt-1 mt-sm-0">
             @if(method_exists($data, 'links'))
                 {!! $data->links(powerGridThemeRoot().'.pagination', ['recordCount' => $recordCount]) !!}
             @endif
         </div>
     </footer>
-    <style>
-        @media (min-width: 992px) {
-            .bs5-per-page {
-                height: 38px
-            }
-        }
-    </style>
+  
+
 @endif
