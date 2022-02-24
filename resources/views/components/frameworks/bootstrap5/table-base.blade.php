eadddf1415c173d8edccc5271ba2649ac731457d
Luan Freitas <luanfreitasgoncalves@gmail.com>
2022-02-24 18:57:40 -0300

    wip bootstrap

--- resources/views/components/frameworks/bootstrap5/table-base.blade.php
+++ resources/views/components/frameworks/bootstrap5/table-base.blade.php
@@ -5,15 +5,15 @@
                     'enabledFilters' => $enabledFilters
             ])
 
-        @includeIf($theme->layout->message)
-    </div>
-    <div class="table-responsive col-md-12" style="margin: 10px 0 10px;">
-        @include($table)
-    </div>
-    <div class="row">
-        <div class="col-12 overflow-auto">
-            @include($theme->footer->view, ['theme' => $theme])
+            @includeIf($theme->layout->message)
+        </div>
+        <div class="table-responsive col-md-12" style="margin: 10px 0 10px;">
+            @include($table)
+        </div>
+        <div class="row">
+            <div class="col-12 overflow-auto">
+                @include($theme->footer->view, ['theme' => $theme])
+            </div>
         </div>
     </div>
 </div>
-
