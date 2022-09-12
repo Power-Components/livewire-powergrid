48c5674fdfe3c789ab93e4f3b48534bf3c7c3b21
Daniel Ang <daniel@sysanalyst.eu>
2022-09-12 14:23:35 +0200

    Add tests for CSV Separator/Delimiter
    - Move CSV expectation inside its test

--- tests/Feature/ExportTest.php
+++ tests/Feature/ExportTest.php
@@ -37,6 +37,16 @@ it('properly export csv data with selected data', function () {
         ->assertFileDownloaded('export.csv');
 });
 
+it('properly sets CSV separator and delimiter', function () {
+    $component = livewire(ExportTable::class, ['separator' => '|', 'delimiter' => '@'])
+       ->set('checkboxValues', [
+           0 => '1',
+       ])
+       ->call('exportToCsv', true);
+
+    expect($component)->toBeCsvWithContent('export.csv', 'ID|Prato1|@Pastel de Nata@');
+});
+
 it('properly export csv - all data', function () {
     livewire(ExportTable::class)
         ->call('exportToCsv', false)
@@ -48,4 +58,31 @@ it('properly does not export csv data without selected data', function () {
         ->call('exportToCsv', true);
 
     expect(null)->notToBeFileDownloaded($component);
+});
+
+/*
+|--------------------------------------------------------------------------
+| Expectations for this test
+|--------------------------------------------------------------------------
+|
+*/
+
+expect()->extend('notToBeFileDownloaded', function ($component) {
+    $downloadEffect = data_get($component->lastResponse, 'original.effects.download');
+
+    expect($downloadEffect)->toBeNull();
+});
+
+expect()->extend('toBeCsvWithContent', function (string $filename, $content) {
+    $downloadEffect = data_get($this->value->lastResponse, 'original.effects.download');
+
+    test()->assertEquals(
+        $filename,
+        data_get($downloadEffect, 'name')
+    );
+
+    test()->assertEquals(
+        preg_replace('/[^A-Za-z0-9.!?|@]/', '', $content),
+        preg_replace('/[^A-Za-z0-9.!?|@]/', '', base64_decode(data_get($downloadEffect, 'content'))),
+    );
 });
