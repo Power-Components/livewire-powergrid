121cacc5a363970f6036a921e7aedebfdba0fbcf
fahedtms <fahed@transportsystemsgroup.com>
2022-10-18 22:20:29 +0400

    add translation

---
+++ resources/lang/ar/datatable.php
@@ -0,0 +1,64 @@
+<?php
+
+return [
+    'buttons' => [
+        'filter'            => 'تصفية',
+        'clear_all_filters' => 'مسح الكل',
+    ],
+    'labels' => [
+        'action'           => 'العمليات',
+        'results_per_page' => 'عدد السجلات في الصفحة',
+        'clear_filter'     => 'مسح الفلاتر',
+        'no_data'          => 'لا يوجد اي سجلات',
+        'all'              => 'الكل',
+        'selected'         => 'محدد',
+    ],
+    'placeholders' => [
+        'search' => 'البحث...',
+        'select' => 'اختر فترة',
+    ],
+    'pagination' => [
+        'showing' => 'يتم عرض',
+        'to'      => 'الى',
+        'of'      => 'من',
+        'results' => 'النتائج',
+        'all'     => 'الكل',
+    ],
+    'multi_select' => [
+        'select' => 'اختيار',
+        'all'    => 'الكل',
+    ],
+    'select' => [
+        'select' => 'اختيار',
+        'all'    => 'الكل',
+    ],
+    'boolean_filter' => [
+        'all' => 'الكل',
+    ],
+    'input_text_options' => [
+        'is'           => 'يساوي',
+        'is_not'       => 'لا يساوي',
+        'contains'     => 'يحتوي',
+        'contains_not' => 'لا يحتوي',
+        'starts_with'  => 'يبدأ ب',
+        'ends_with'    => 'ينتهي ب',
+        'is_empty'     => 'فارغ',
+        'is_not_empty' => 'غير فارغ',
+        'is_null'      => 'غير مهيئ',
+        'is_not_null'  => 'ليس غير مهيئ',
+        'is_blank'     => 'فراغ',
+        'is_not_blank' => 'ليس فراغ',
+
+    ],
+    'export' => [
+        'exporting' => 'الرجاء الانتظار!',
+        'completed' => 'تم الانتهاء من التصدير، الملف جاهز للتحميل',
+    ],
+    'soft_deletes' => [
+        'message_with_trashed' => 'عرض جميع السجلات بما فيها المحذوفة',
+        'message_only_trashed' => 'عرض فقط السجلات المحذوفة',
+        'without_trashed'      => 'بدون المحذوف',
+        'with_trashed'         => 'مع المحذوف',
+        'only_trashed'         => 'فقط محذوف',
+    ],
+];
