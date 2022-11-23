30e10afc9477ee32068126fddd7bb15106d324c5
Jan Korbay <korbay.jan@gmail.com>
2022-11-23 20:38:01 +0100

    Added Czech language translate

---
+++ resources/lang/cs/datatable.php
@@ -0,0 +1,64 @@
+<?php
+
+return [
+    'buttons' => [
+        'filter'            => 'Filtrovat',
+        'clear_all_filters' => 'Vymazat vše',
+    ],
+    'labels' => [
+        'action'           => 'Akce',
+        'results_per_page' => 'Záznamy na stránku',
+        'clear_filter'     => 'Vymazat filtry',
+        'no_data'          => 'Nebyly nalezeny žádné záznamy',
+        'all'              => 'Všechny',
+        'selected'         => 'Vybrané',
+    ],
+    'placeholders' => [
+        'search' => 'Hledat...',
+        'select' => 'Vyberte období',
+    ],
+    'pagination' => [
+        'showing' => 'Zobrazeno',
+        'to'      => 'až',
+        'of'      => 'z',
+        'results' => 'Výsledků',
+        'all'     => 'Všechny',
+    ],
+    'multi_select' => [
+        'select' => 'Vybrat',
+        'all'    => 'Všechny',
+    ],
+    'select' => [
+        'select' => 'Vybrat',
+        'all'    => 'Všechny',
+    ],
+    'boolean_filter' => [
+        'all' => 'Všechny',
+    ],
+    'input_text_options' => [
+        'is'           => 'Je',
+        'is_not'       => 'Není',
+        'contains'     => 'Obsahuje',
+        'contains_not' => 'Neobsahuje',
+        'starts_with'  => 'Začíná na',
+        'ends_with'    => 'Končí na',
+        'is_empty'     => 'Je prázdný',
+        'is_not_empty' => 'Není prázdný',
+        'is_null'      => 'Je nulový',
+        'is_not_null'  => 'Není nulový',
+        'is_blank'     => 'Je prázdný',
+        'is_not_blank' => 'Není prázdný',
+
+    ],
+    'export' => [
+        'exporting' => 'Počkejte prosím!',
+        'completed' => 'Export dokončen! Vaše soubory jsou připraveny ke stažení',
+    ],
+    'soft_deletes' => [
+        'message_with_trashed' => 'Zobrazení všech záznamů, včetně smazaných.',
+        'message_only_trashed' => 'Zobrazení pouze smazaných záznamů.',
+        'without_trashed'      => 'Bez smazaných záznamů',
+        'with_trashed'         => 'Se smazanými',
+        'only_trashed'         => 'Pouze smazané',
+    ],
+];
