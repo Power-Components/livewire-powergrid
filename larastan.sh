2a728e2f31f0f3ad85a02c1937992f9d20517c1f
Luan Freitas <luanfreitasgoncalves@gmail.com>
2023-01-03 16:36:55 -0300

    remove larastan.sh

--- larastan.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-LARAVEL_VERSION="$(composer show 'laravel/framework' --format=json | jq -r '."versions"[0] | sub("v";"")')"
-
-if { echo "9.0.0"; echo "${LARAVEL_VERSION}"; } | sort --version-sort --check 2>/dev/null; then
-  ./vendor/bin/phpstan analyse --ansi --memory-limit=-1
-fi
