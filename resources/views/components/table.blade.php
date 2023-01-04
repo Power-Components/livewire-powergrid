a72fa4c27cee51f4569299260f74f93e30e3968d
Luan Freitas <luanfreitasgoncalves@gmail.com>
2023-01-04 16:49:35 -0300

    feat: add deferLoading

--- resources/views/components/table.blade.php
+++ resources/views/components/table.blade.php
@@ -1,7 +1,7 @@
 @inject('actionRulesClass','PowerComponents\LivewirePowerGrid\Helpers\ActionRules')
 
-<x-livewire-powergrid::table-base :theme="$theme->table">
-    <x-slot name="header">
+<x-livewire-powergrid::table-base :theme="$theme->table" :ready-to-load="$readyToLoad">
+    <x-slot:header>
         <tr class="{{ $theme->table->trClass }}" style="{{ $theme->table->trStyle }}">
             @if(data_get($setUp, 'detail.showCollapseIcon'))
                 <th scope="col" class="{{ $theme->table->thClass }}"
@@ -35,9 +35,24 @@
                 </th>
             @endif
         </tr>
-    </x-slot>
+    </x-slot:header>
 
-    <x-slot name="rows">
+    <x-slot:loading>
+        <tr class="{{ $theme->table->trBodyClass }}" style="{{ $theme->table->trBodyStyle }}">
+            <td class="{{ $theme->table->tdBodyEmptyClass }}" colspan="{{ (($checkbox) ? 1:0)
+                   + ((isset($actions)) ? 1: 0)
+                   + (count($columns))
+                }}">
+                @if($loadingComponent)
+                    @include($loadingComponent)
+                @else
+                    {{ __('Loading') }}
+                @endif
+            </td>
+        </tr>
+    </x-slot:loading>
+
+    <x-slot:rows>
         <x-livewire-powergrid::inline-filters
             :checkbox="$checkbox"
             :actions="$actions"
@@ -151,5 +166,5 @@
 
             @includeWhen($footerTotalColumn, 'livewire-powergrid::components.table-footer')
         @endif
-    </x-slot>
+    </x-slot:rows>
 </x-livewire-powergrid::table-base>
