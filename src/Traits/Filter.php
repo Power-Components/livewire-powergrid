b98c7a71398de03a45f799a178d33a9311866956
Luan Freitas <33601626+luanfreitasdev@users.noreply.github.com>
2023-01-21 15:48:45 -0300

    Bugfix/Clear range input not working with column ID with joins (#826)
    * Update Filter.php
    
    * cs fix

--- src/Traits/Filter.php
+++ src/Traits/Filter.php
@@ -34,6 +34,15 @@ trait Filter
             unset($this->filters['input_date_picker'][$table][$column]);
             unset($this->filters['select'][$table][$column]);
             unset($this->filters['multi_select'][$table][$column]);
+
+            unset($this->filters['input_text'][$table . '.' . $column]);
+            unset($this->filters['input_text_options'][$table . '.' . $column]);
+            unset($this->filters['number'][$table . '.' . $column]['start']);
+            unset($this->filters['number'][$table . '.' . $column]['end']);
+            unset($this->filters['boolean'][$table . '.' . $column]);
+            unset($this->filters['select'][$table . '.' . $column]);
+            unset($this->filters['multi_select'][$table . '.' . $column]);
+
             if (empty($this->filters['input_text'][$table])) {
                 unset($this->filters['input_text'][$table]);
             }
