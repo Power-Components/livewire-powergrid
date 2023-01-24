f191e88915b463f4d7994983e29359659c0fd104
Paweł Serafin incron <pawel.serafin@incron.pl>
2023-01-24 17:22:11 +0100

    Add polish translation

---
+++ resources/lang/pl/datatable.php
@@ -0,0 +1,66 @@
+<?php
+
+return [
+    'buttons'            => [
+        'filter'            => 'Filtry',
+        'clear_all_filters' => 'Wyczyść wszystkie filtry',
+    ],
+    'labels'             => [
+        'action'           => 'Akcje',
+        'results_per_page' => 'Pozycji na stronę',
+        'clear_filter'     => 'Wyczyść filtr',
+        'no_data'          => 'Nie znaleziono pasujących pozycji',
+        'all'              => 'Wszystkie',
+        'selected'         => 'Zaznaczone',
+    ],
+    'placeholders'       => [
+        'search' => 'Szukaj...',
+        'select' => 'Wybierz okres',
+    ],
+    'pagination'         => [
+        'showing' => 'Pozycje',
+        'to'      => 'do',
+        'of'      => 'z',
+        'results' => 'dostępnych',
+        'all'     => 'Wszystko',
+    ],
+    'multi_select'       => [
+        'select' => 'Wybierz',
+        'all'    => 'Wszystko',
+    ],
+    'select'             => [
+        'select' => 'Wybierz',
+        'all'    => 'Wszystko',
+    ],
+    'boolean_filter'     => [
+        'all' => 'Wszystko',
+    ],
+    'input_text_options' => [
+        'is'           => 'Jest równy',
+        'is_not'       => 'Nie jest równy',
+        'contains'     => 'Zawiera',
+        'contains_not' => 'Nie zawiera',
+        'starts_with'  => 'Zaczyna się od',
+        'ends_with'    => 'Kończy się na',
+        'is_empty'     => 'Jest pusty',
+        'is_not_empty' => 'Nie jest pusty',
+        'is_null'      => 'Jest pusty (null)',
+        'is_not_null'  => 'Nie pusty (null)',
+        'is_blank'     => 'Jest pusty (blank)',
+        'is_not_blank' => 'Nie jest pusty (blank)',
+    ],
+    'export'             => [
+        'exporting' => 'Proszę czekać!',
+        'completed' => 'Eksport gotowy! Twoje pliki są gotowe do pobrania',
+    ],
+    'soft_deletes'       => [
+        'message_with_trashed' => 'Wyświetlam wszystkie pozycję, uwzględniając skasowane.',
+        'message_only_trashed' => 'Wyświetlam tylko pozycję w koszu.',
+        'without_trashed'      => 'Bez skasowanych',
+        'with_trashed'         => 'Uwzględnij kosz',
+        'only_trashed'         => 'Pokaż tylko kosz',
+    ],
+    'multi_sort'         => [
+        'message' => 'Wielokolumnowe sortowanie jest aktywne',
+    ],
+];
