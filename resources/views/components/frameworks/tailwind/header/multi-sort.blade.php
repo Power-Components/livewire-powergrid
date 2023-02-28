9cf38831db725dbe18f4a1e5c71faebbb5141c5a
Rafael-devops <59840647+Rafael-devops@users.noreply.github.com>
2023-02-28 14:13:34 -0300

    Add class items-center in message (#870)

--- resources/views/components/frameworks/tailwind/header/multi-sort.blade.php
+++ resources/views/components/frameworks/tailwind/header/multi-sort.blade.php
@@ -1,5 +1,5 @@
 @if($multiSort && count($sortArray) > 0)
-    <div class="w-full flex pt-3 mb-3 gap-2">
+    <div class="w-full items-center flex pt-3 mb-3 gap-2">
         <span class="dark:text-pg-primary-300">@lang('livewire-powergrid::datatable.multi_sort.message')</span>
         <span class="flex gap-2 select-none">
             @foreach($sortArray as $field => $sort)
