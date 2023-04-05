072f3b56268c34876962f4add48a5cacd080f6af
MarkasCherry <markas.cherry@gmail.com>
2023-04-05 14:23:54 +0300

    Added details count to colspan calculation

--- resources/views/components/table.blade.php
+++ resources/views/components/table.blade.php
@@ -54,6 +54,7 @@
                     <td class="{{ $theme->table->tdBodyClass }}" style="{{ $theme->table->tdBodyStyle }}" colspan="{{ (($checkbox) ? 1:0)
                                     + ((isset($actions)) ? 1: 0)
                                     + (count($columns))
+                                    + (data_get($setUp, 'detail.showCollapseIcon') ? 1: 0)
                                     }}">
                         <span>{{ trans('livewire-powergrid::datatable.labels.no_data') }}</span>
                     </td>
