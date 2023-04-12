0e94bd7aa0488f569433d228e5b81ffe762ccb05
jamesRUS52 <39707635+jamesRUS52@users.noreply.github.com>
2023-04-12 12:11:36 +0300

    Fix number filter for bootstrap (#920)
    Fix empty text on clear filter button
    Fix clear values in text field after clear button clicked
    Prevent text input to fields (only numbers)

--- resources/views/components/frameworks/bootstrap5/filters/number.blade.php
+++ resources/views/components/frameworks/bootstrap5/filters/number.blade.php
@@ -10,20 +10,20 @@
             <div>
                 <input
                     data-id="{{ data_get($number, 'field') }}"
-                    wire:model.debounce.800ms="filters.number_start.{{ data_get($number, 'dataField') }}"
-                    wire:input.debounce.800ms="filterNumberStart('{{ data_get($number, 'dataField') }}', $event.target.value,'{{ addslashes(data_get($number, 'thousands')) }}','{{ addslashes(data_get($number, 'decimal')) }}','{{ data_get($number, 'label') }}')"
+                    wire:model.debounce.800ms="filters.number.{{ data_get($number, 'dataField') }}.start"
+                    wire:input.debounce.800ms="filterNumberStart('{{ data_get($number, 'dataField') }}', $event.target.value,'{{ data_get($number, 'label') }}')"
                     @if($inline) style="{{ $theme->inputStyle }} {{ data_get($column, 'headerStyle') }}" @endif
-                    type="text"
+                    type="number"
                     class="power_grid {{ $theme->inputClass }} {{ data_get($column, 'headerClass') }}"
                     placeholder="Min">
             </div>
             <div class="mt-1">
                 <input
                     data-id="{{ $number['field'] }}"
-                    wire:model.debounce.800ms="filters.number_end.{{ data_get($number, 'dataField') }}"
-                    wire:input.debounce.800ms="filterNumberEnd('{{ data_get($number, 'dataField') }}',$event.target.value,'{{ addslashes(data_get($number, 'thousands')) }}','{{ addslashes(data_get($number, 'decimal')) }}', '{{ data_get($number, 'label') }}')"
+                    wire:model.debounce.800ms="filters.number.{{ data_get($number, 'dataField') }}.end"
+                    wire:input.debounce.800ms="filterNumberEnd('{{ data_get($number, 'dataField') }}',$event.target.value,'{{ data_get($number, 'label') }}')"
                     @if($inline) style="{{ $theme->inputStyle }} {{ data_get($column, 'headerStyle') }}" @endif
-                    type="text"
+                    type="number"
                     class="power_grid {{ $theme->inputClass }} {{ data_get($column, 'headerClass') }}"
                     placeholder="Max">
             </div>
