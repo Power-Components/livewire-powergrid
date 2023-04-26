e3e048db114314a4ac9634e85d1c53307b094a44
luanfreitasdev <luanfreitas10@protonmail.com>
2023-04-26 17:51:32 -0300

    fix flatpickr bug

--- src/Traits/HasFilter.php
+++ src/Traits/HasFilter.php
@@ -183,8 +183,8 @@ trait HasFilter
         $endDate   = Carbon::createFromFormat('Y-m-d', $endDate, $filterTimezone);
 
         if ($data['type'] === 'datetime') {
-            $startDate->setTime(0, 0, 0)->setTimeZone($appTimeZone);
-            $endDate->setTime(23, 59, 59)->setTimeZone($appTimeZone);
+            $startDate->setTimeZone($appTimeZone);
+            $endDate->setTimeZone($appTimeZone);
         }
 
         data_set($data, 'selectedDates.0', $startDate);
