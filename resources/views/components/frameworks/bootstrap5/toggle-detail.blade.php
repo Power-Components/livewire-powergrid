20e72d1dfaa12005b929fb16897418d9b2cb5435
bruno <bruno@brunosalzano.com>
2023-05-30 00:55:01 +0200

    Changed CSS: added bs5 classes for rotate and sizing toggle-detail.blade.php: improved collapse code with one component (like in tailwind theme)

--- resources/views/components/frameworks/bootstrap5/toggle-detail.blade.php
+++ resources/views/components/frameworks/bootstrap5/toggle-detail.blade.php
@@ -9,8 +9,10 @@
         @includeIf(data_get($setUp, 'detail.viewIcon'))
 
         @if (!data_get($setUp, 'detail.viewIcon'))
-            <x-livewire-powergrid::icons.arrow x-show="!detailState" />
-            <x-livewire-powergrid::icons.chevron-down x-show="detailState" />
+            <x-livewire-powergrid::icons.arrow
+                    x-bind:class="detailState ? 'bs5-rotate-90' : 'bs5-rotate-0'"
+                    class="bs5-w-h-1_5em"
+            />
         @endif
     </div>
 </td>
