2a4961d94dd3a07273ce6ae8ea73fcc1d6f6cb8e
Luan Freitas <33601626+luanfreitasdev@users.noreply.github.com>
2023-06-09 16:52:39 -0300

    fix checkbox column (#1025)

--- resources/views/components/checkbox-all.blade.php
+++ resources/views/components/checkbox-all.blade.php
@@ -2,7 +2,13 @@
     'theme' => null,
 ])
 <div>
+    @php
+        $responsiveCheckboxColumnName = PowerComponents\LivewirePowerGrid\Responsive::CHECKBOX_COLUMN_NAME;
+
+        $isCheckboxFixedOnResponsive = isset($this->setUp['responsive']) && in_array($responsiveCheckboxColumnName, data_get($this->setUp, 'responsive.fixedColumns')) ? true : false;
+    @endphp
     <th
+        @if($isCheckboxFixedOnResponsive) fixed @endif
         scope="col"
         class="{{ $theme->thClass }}"
         style="{{ $theme->thStyle }}"
