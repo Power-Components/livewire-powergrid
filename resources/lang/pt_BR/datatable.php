3e1aa65c2d5395b34a6cac2842b99a6e5a040ead
luanfreitasdev <luanfreitas10@protonmail.com>
2023-07-22 14:17:41 -0300

    fix pt-BR

--- resources/lang/pt_BR/datatable.php
+++ resources/lang/pt_BR/datatable.php
@@ -12,7 +12,7 @@ return [
         'no_data'          => 'Nenhum registro encontrado',
         'all'              => 'Todos',
         'selected'         => 'Selecionados',
-        'filtered'         => 'Filtrado',
+        'filtered'         => 'Filtrados',
     ],
     'placeholders' => [
         'search' => 'Buscar ...',
