3f7eb5b39aa1ac48c74cee8ac39cb6adee5ff62e
luanfreitasdev <luanfreitas10@protonmail.com>
2023-09-17 17:59:19 -0300

    wip

--- tests/cypress/run.sh
+++ tests/cypress/run.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-git clone https://github.com/Power-Components/powergrid-demo-misc.git app &
-
 cd app &&
 
 # | ------------------------- |
@@ -18,18 +16,6 @@ DB_DATABASE=powergridtest
 DB_USERNAME=root
 DB_PASSWORD=password
 EOF
-
-# | ------------------------- |
-# | copy stubs
-# | ------------------------- |
-
-ls -lh
-
-sudo chmod -R 755 app
-sudo chmod -R 755 resources
-
-cp tests/cypress/stubs/CypressTable.php tests/cypress/app/app/Livewire/CypressTable.php
-cp tests/cypress/stubs/rules-controllers.blade.php tests/cypress/app/resources/views/components/header/rules-controllers.blade.php
 
 # | ------------------------- |
 # | install local PowerGrid
