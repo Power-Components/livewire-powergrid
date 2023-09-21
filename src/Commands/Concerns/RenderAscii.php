e7a0999a951ac7130f9bda3c1982aa43f5646261
Luan Freitas <33601626+luanfreitasdev@users.noreply.github.com>
2023-09-21 19:45:04 -0300

    add laravel prompt (#1163)

--- src/Commands/Concerns/RenderAscii.php
+++ src/Commands/Concerns/RenderAscii.php
@@ -9,17 +9,14 @@ trait RenderAscii
      */
     public function renderPowergridAscii(): void
     {
-        $this->newLine();
-
         $this->line(<<<EOT
-
-            <fg=yellow> __</>     <fg=green>____                          ______     _     __</>
-            <fg=yellow>/ /_,</>  <fg=green>/ __ \____ _      _____  _____/ ____/____(_)___/ /</>
-            <fg=yellow>/_ ,'</> <fg=green>/ /_/ / __ \ | /| / / _ \/ ___/ / __/ ___/ / __  / </>
+            <fg=yellow;options=bold> __</>     <fg=green>____                          ______     _     __</>
+            <fg=yellow;options=bold>/ /_,</>  <fg=green>/ __ \____ _      _____  _____/ ____/____(_)___/ /</>
+            <fg=yellow;options=bold>/_ ,'</> <fg=green>/ /_/ / __ \ | /| / / _ \/ ___/ / __/ ___/ / __  / </>
             <fg=yellow>/'</>   <fg=green>/ ____/ /_/ / |/ |/ /  __/ /  / /_/ / /  / / /_/ /  </>
                 <fg=green>/_/    \____/|__/|__/\___/_/   \____/_/  /_/\__,_/   </>  
         EOT);
 
-        $this->newLine(2);
+        $this->newLine();
     }
 }
