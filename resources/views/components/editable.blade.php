b08c7533a6dfe9ec111c73af5515018039591917
Luan Freitas <33601626+luanfreitasdev@users.noreply.github.com>
2023-09-28 15:52:43 -0300

    add wire:key (#1186)

--- resources/views/components/editable.blade.php
+++ resources/views/components/editable.blade.php
@@ -13,28 +13,28 @@
     $resolveContent = function (string $currentTable, string $field, \Illuminate\Database\Eloquent\Model|\stdClass $row): ?string {
         $currentField = $field;
         $replace = fn($content) => preg_replace('#<script(.*?)>(.*?)</script>#is', '', $content);
-    
+
         /** @codeCoverageIgnore */
         if (str_contains($currentField, '.')) {
             $data = \Illuminate\Support\Str::of($field)->explode('.');
             $table = $data->get(0);
             $field = $data->get(1);
-    
+
             if ($table === $currentTable) {
                 return $replace($row->{$field});
             }
-    
+
             return $replace($row->{$table}->{$field});
         }
-    
+
         return $replace($row->{$field});
     };
-    
+
     $fallback = html_entity_decode(strval(data_get($editable, 'fallback')), ENT_QUOTES, 'utf-8');
     $value = html_entity_decode(strval($resolveContent($currentTable, $field, $row)), ENT_QUOTES, 'utf-8');
-    
+
     $content = !empty($value) || $value == '0' ? $value : $fallback;
-    
+
     $params = [
         'theme' => $theme->name,
         'tableName' => $tableName,
@@ -45,6 +45,7 @@
     ];
 @endphp
 <div
+    wire:key="editable-{{ uniqid() }}"
     x-cloak
     x-data="pgEditable(@js($params))"
     style="width: 100% !important; height: 100% !important;"
