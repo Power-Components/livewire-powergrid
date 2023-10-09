648ff651616df594c3a24b2e7a217579eeebabdc
Luan Freitas <33601626+luanfreitasdev@users.noreply.github.com>
2023-10-09 17:10:00 -0300

    Fix flatpickr lose text value (#1203)
    * fix flatpickr lose text value
    
    * prevent requests on close

--- resources/js/components/pg-flatpickr.js
+++ resources/js/components/pg-flatpickr.js
@@ -1,7 +1,6 @@
 export default (params) => ({
     dataField: params.dataField,
     tableName: params.tableName,
-    filterKey: params.filterKey,
     label: params.label ?? null,
     locale: params.locale ?? 'en',
     onlyFuture: params.onlyFuture ?? false,
@@ -9,19 +8,17 @@ export default (params) => ({
     customConfig: params.customConfig ?? null,
     type: params.type,
     element: null,
-    lastRequest: null,
+    selectedDates: null,
     init() {
         window.addEventListener(`pg:clear_flatpickr::${this.tableName}:${this.dataField}`, () => {
             if (this.$refs.rangeInput && this.element) {
                 this.element.clear()
-                this.lastRequest = null;
             }
         })
 
         window.addEventListener(`pg:clear_all_flatpickr::${this.tableName}`, () => {
             if (this.$refs.rangeInput && this.element) {
                 this.element.clear()
-                this.lastRequest = null;
             }
         })
 
@@ -35,6 +32,10 @@ export default (params) => ({
 
         if(this.$refs.rangeInput) {
             this.element = flatpickr(this.$refs.rangeInput, options);
+
+            this.selectedDates = this.$wire.get(`filters.${this.type}.${this.dataField}.formatted`)
+
+            this.element.setDate(this.selectedDates)
         }
     },
     getOptions() {
@@ -58,18 +59,16 @@ export default (params) => ({
         }
 
         options.onClose = (selectedDates, dateStr, instance) => {
+
             selectedDates = selectedDates.map((date) => this.element.formatDate(date, 'Y-m-d'));
 
-            const key = `${this.tableName}::${this.dataField}::${selectedDates}`
-            const encrypted = Buffer.from(key).toString('base64')
-
-            if (selectedDates.length > 0 && encrypted !== this.lastRequest) {
+            if (selectedDates.length > 0 && (this.selectedDates !== dateStr)) {
                 Livewire.dispatch('pg:datePicker-' + this.tableName, {
                     selectedDates: selectedDates,
+                    dateStr,
                     timezone: this.customConfig.timezone ?? new Date().toString().match(/([-\+][0-9]+)\s/)[1],
                     type: this.type,
                     field: this.dataField,
-                    wireModel: this.filterKey,
                     label: this.label,
                 });
             }
