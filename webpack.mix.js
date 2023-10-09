c343ad90ccc7a14a2b848bfd139950f601126af9
Luan Freitas <33601626+luanfreitasdev@users.noreply.github.com>
2023-10-09 17:09:12 -0300

    separates tailwind and bootstrap css (#1202)

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,6 +3,8 @@ const mix = require('laravel-mix')
 mix.js('resources/js/index.js', 'powergrid.js')
     .css('resources/css/style.css', 'powergrid.css')
     .css('resources/css/tom-select.css', 'tom-select.css')
+    .css('resources/css/tailwind.css', 'tailwind.css')
+    .css('resources/css/bootstrap.css', 'bootstrap5.css')
     .setPublicPath('dist')
 
 if (mix.inProduction()) {
