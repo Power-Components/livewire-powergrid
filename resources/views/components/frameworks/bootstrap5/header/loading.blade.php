5fb8f2d47027b32e18373d90ff8240105fae994a
Ssenoga Edward <senoeddie@gmail.com>
2023-10-16 22:22:32 +0300

    Added hide classes on the loader

--- resources/views/components/frameworks/bootstrap5/header/loading.blade.php
+++ resources/views/components/frameworks/bootstrap5/header/loading.blade.php
@@ -1,5 +1,5 @@
 <div class="d-flex align-items-center" style="padding-left: 10px;">
-    <div wire:loading class="spinner-border" role="status" style="color: #656363;">
+    <div wire:loading class="spinner-border d-none" role="status" style="color: #656363;">
         <span class="visually-hidden">Loading...</span>
     </div>
 </div>
