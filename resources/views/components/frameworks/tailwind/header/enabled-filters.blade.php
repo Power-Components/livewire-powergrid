f0f54ad64c947a3e2224d341c24e8dad221f2c0e
Luan Freitas <33601626+luanfreitasdev@users.noreply.github.com>
2023-11-05 09:51:14 -0300

    add classes (#1242)
    * add classes
    
    * build

--- resources/views/components/frameworks/tailwind/header/enabled-filters.blade.php
+++ resources/views/components/frameworks/tailwind/header/enabled-filters.blade.php
@@ -1,9 +1,7 @@
 @if (count($enabledFilters))
-    <div class="flex items-center gap-2 mt-2 md:mt-0 flex-wrap">
+    <div class="pg-enabled-filters-base">
         @if (count($enabledFilters) > 1)
-            <span
-                class="outline-none inline-flex justify-center items-center group rounded gap-x-1 text-xs font-semibold px-2.5 py-0.5 text-pg-primary-100 bg-pg-primary-500 dark:bg-pg-primary-700"
-            >
+            <span class="group pg-enabled-filters-clear-all">
                 {{ trans('livewire-powergrid::datatable.buttons.clear_all_filters') }}
                 <div class="relative flex items-center w-2 h-2">
                     <button
@@ -19,7 +17,7 @@
             @isset($filter['label'])
                 <span
                     wire:key="enabled-filters-{{ $field }}"
-                    class="outline-none inline-flex justify-center items-center group rounded gap-x-1 text-xs font-semibold px-2.5 py-0.5 text-pg-primary-600 dark:text-pg-primary-200 bg-pg-primary-100 dark:bg-pg-primary-600"
+                    class="group pg-enabled-filters-span"
                 >
                     {{ $filter['label'] }}
                     <div class="relative flex items-center w-2 h-2">
