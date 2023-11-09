eac83c03e2619ffba0df4788eb637ad03fa4166b
Marc Geurts <marc.geurts@scarlet.be>
2023-11-09 14:42:08 +0100

    Add Dutch language (#1247)

---
+++ resources/lang/nl/datatable.php
@@ -0,0 +1,67 @@
+<?php
+
+return [
+    'buttons' => [
+        'filter'            => 'Filter',
+        'clear_all_filters' => 'Wis alle filters',
+    ],
+    'labels' => [
+        'action'           => 'Acties',
+        'results_per_page' => 'Records per pagina',
+        'clear_filter'     => 'Wis filter',
+        'no_data'          => 'Geen records gevonden',
+        'all'              => 'Alles',
+        'selected'         => 'Geselecteerd',
+        'filtered'         => 'Gefilterd',
+    ],
+    'placeholders' => [
+        'search' => 'Zoek...',
+        'select' => 'Selecteer een periode',
+    ],
+    'pagination' => [
+        'showing' => 'Toont',
+        'to'      => 'tot',
+        'of'      => 'van',
+        'results' => 'Resultaten',
+        'all'     => 'Alles',
+    ],
+    'multi_select' => [
+        'select' => 'Selecteer',
+        'all'    => 'Alles',
+    ],
+    'select' => [
+        'select' => 'Selecteer',
+        'all'    => 'Alles',
+    ],
+    'boolean_filter' => [
+        'all' => 'Alles',
+    ],
+    'input_text_options' => [
+        'is'           => 'Is',
+        'is_not'       => 'Is not',
+        'contains'     => 'Bevat',
+        'contains_not' => 'Bevat geen',
+        'starts_with'  => 'Begint met',
+        'ends_with'    => 'Eindigt met',
+        'is_empty'     => 'Is leeg',
+        'is_not_empty' => 'Is niet leeg',
+        'is_null'      => 'Is null',
+        'is_not_null'  => 'Is niet null',
+        'is_blank'     => 'Is niet ingevuld',
+        'is_not_blank' => 'Is ingevuld',
+    ],
+    'export' => [
+        'exporting' => 'Even geduld a.u.b.!',
+        'completed' => 'Export voltooid! Uw bestaanden staan klaar voor download.',
+    ],
+    'soft_deletes' => [
+        'message_with_trashed' => 'Toont alle records, inclusief verwijderde.',
+        'message_only_trashed' => 'Toont enkele verwijderde records.',
+        'without_trashed'      => 'Exclusief verwijderde',
+        'with_trashed'         => 'Inclusief verwijderde',
+        'only_trashed'         => 'Enkel verwijderde',
+    ],
+    'multi_sort' => [
+        'message' => 'Meervoudige sortering is actief',
+    ],
+];
