7d8ad8d2d68569ba0ab1f18fb08287c8e09b0f6b
luanfreitasdev <luanfreitas10@protonmail.com>
2023-11-11 11:20:40 -0300

    fix slimSelect settings

--- resources/js/components/select/slimSelect.js
+++ resources/js/components/select/slimSelect.js
@@ -1,20 +1,42 @@
 import { storeMultiSelect } from "./store";
 
 export default (params) => ({
+  initialValues: params.initialValues,
+  framework: params.framework,
   init() {
-    console.log(params);
     const element =
-      this.$refs["select_picker_" + params.dataField + "_" + params.tableName];
+        this.$refs["select_picker_" + params.dataField + "_" + params.tableName];
+
+    const frameworkCopy = this.deepCopy(this.framework);
 
     new window.SlimSelect({
       select: element,
-      data: this.initialValues,
-      ...this.framework,
+      ...frameworkCopy,
       events: {
         afterChange: (value) => {
-          storeMultiSelect(params, value);
+          let newValue = value.map(item => item.value);
+          storeMultiSelect(params, newValue);
         },
       },
     });
   },
+  deepCopy(obj) {
+    if (obj === null || typeof obj !== 'object') {
+      return obj;
+    }
+
+    if (Array.isArray(obj)) {
+      return obj.map(this.deepCopy);
+    }
+
+    const copy = {};
+    for (const key in obj) {
+      if (obj.hasOwnProperty(key)) {
+        copy[key] = this.deepCopy(obj[key]);
+      }
+    }
+
+    return copy;
+  }
 });
+
