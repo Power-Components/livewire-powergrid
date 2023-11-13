8c94212fc94baffed995a32fc1129d98f18815b6
Luan Freitas <33601626+luanfreitasdev@users.noreply.github.com>
2023-11-13 12:38:51 -0300

    rename tableResponsive to pgResponsive (#1254)

--- resources/js/components/index.js
+++ resources/js/components/index.js
@@ -1,19 +1,19 @@
 import pgToggleable from './pg-toggleable'
 import pgFlatpickr from "./pg-flatpickr";
 import pgEditable from "./pg-editable";
-import pgCopyToClipboard from "./pg-copy-to-clipboard"; 
-import tableResponsive from './tableResponsive';
+import pgCopyToClipboard from "./pg-copy-to-clipboard";
+import pgResponsive from './pgResponsive';
 
 window.pgToggleable = pgToggleable
 window.pgFlatpickr = pgFlatpickr
 window.pgEditable = pgEditable
-window.tableResponsive = tableResponsive
+window.pgResponsive = pgResponsive
 
 document.addEventListener('alpine:init', () => {
     window.Alpine.data('pgToggleable', pgToggleable)
     window.Alpine.data('pgFlatpickr', pgFlatpickr)
     window.Alpine.data('phEditable', pgEditable)
-    window.Alpine.data('tableResponsive', tableResponsive)
+    window.Alpine.data('pgResponsive', pgResponsive)
     window.Alpine.plugin(pgCopyToClipboard)
 })
 
