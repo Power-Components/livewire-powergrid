1b43ac721582cf21911226535d785f18d588562c
Александр <flatbusy@gmail.com>
2023-12-21 19:22:36 +0700

    Fix Responsive when ID is UUID (#1305)

--- resources/views/components/frameworks/tailwind/toggle-detail-responsive.blade.php
+++ resources/views/components/frameworks/tailwind/toggle-detail-responsive.blade.php
@@ -10,7 +10,7 @@
     >
         <x-livewire-powergrid::icons.arrow
             class="text-pg-primary-600 w-5 h-5 transition-all duration-300 dark:text-pg-primary-200"
-            x-bind:class="expanded == {{ $rowId }} ? 'rotate-90' : '-rotate-0'"
+            x-bind:class="expanded == '{{ $rowId }}' ? 'rotate-90' : '-rotate-0'"
         />
     </button>
 </td>
