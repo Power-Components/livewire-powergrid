f60d3c2084d5bcf1d239a3dad11183516e8bd005
luanfreitasdev <luanfreitas10@protonmail.com>
2023-12-28 17:06:05 -0300

    Add bg-white dark:bg-pg-primary-900

--- resources/views/components/frameworks/tailwind/table-base.blade.php
+++ resources/views/components/frameworks/tailwind/table-base.blade.php
@@ -4,11 +4,11 @@
 >
     <div
         id="power-grid-table-container"
-        class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8"
+        class="-my-2 overflow-x-auto sm:-mx-6"
     >
         <div
             id="power-grid-table-base"
-            class="py-2 align-middle inline-block min-w-full w-full sm:px-6 lg:px-8"
+            class="py-4 bg-white dark:bg-pg-primary-900 align-middle inline-block min-w-full w-full sm:px-6"
         >
 
             @include($theme->layout->header, [
