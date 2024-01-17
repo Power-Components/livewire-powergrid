786132e09978b49edcb2a3675afa791e5f43cdc7
Luan Freitas <33601626+luanfreitasdev@users.noreply.github.com>
2024-01-17 16:50:57 -0300

    Fix detail row - loading without state (#1333)

--- resources/views/components/table/detail.blade.php
+++ resources/views/components/table/detail.blade.php
@@ -1,19 +1,29 @@
-<td colspan="999">
-    @php
-        $rulesValues = $actionRulesClass->recoverFromAction($row, 'pg:rows');
-    @endphp
-
-    @if (filled($rulesValues['detailView']))
-        @include($rulesValues['detailView'][0]['detailView'], [
-            'id'      => data_get($row, $primaryKey),
-            'options' => array_merge(
-                data_get($setUp, 'detail.options'),
-                $rulesValues['detailView']['0']['options']),
-        ])
-    @else
-        @include(data_get($setUp, 'detail.view'), [
-            'id'      => data_get($row, $primaryKey),
-            'options' => data_get($setUp, 'detail.options'),
-        ])
-    @endif
-</td>
+@php
+    $rulesValues = $actionRulesClass->recoverFromAction($row, 'pg:rows');
+@endphp
+<template
+        x-cloak
+        x-if="detailState"
+>
+    <td colspan="999">
+        @if (filled($rulesValues['detailView']))
+            @includeWhen(data_get($setUp, 'detail.state.' . $row->{$primaryKey}),
+                $rulesValues['detailView'][0]['detailView'],
+                [
+                    'id' => data_get($row, $primaryKey),
+                    'options' => array_merge(
+                        data_get($setUp, 'detail.options'),
+                        $rulesValues['detailView']['0']['options']),
+                ]
+            )
+        @else
+            @includeWhen(data_get($setUp, 'detail.state.' . $row->{$primaryKey}),
+                data_get($setUp, 'detail.view'),
+                [
+                    'id' => data_get($row, $primaryKey),
+                    'options' => data_get($setUp, 'detail.options'),
+                ]
+            )
+        @endif
+    </td>
+</template>
