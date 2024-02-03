0d1dd49c296da02bedd804b4da5b238e6e6c6fca
luanfreitasdev <luanfreitas10@protonmail.com>
2024-02-03 20:30:32 -0300

    fixing number test

--- tests/cypress/cypress/e2e/filters/number.cy.js
+++ tests/cypress/cypress/e2e/filters/number.cy.js
@@ -3,33 +3,63 @@ describe('simple', () => {
         cy.visit('/filters-number');
     });
 
-    // it('filter between 20 - 25', () => {
-    //     cy.get('[wire\\:model="filters.number.price.start"]')
-    //         .type('20').should('have.value', '20');
-    //
-    //     cy.get('[wire\\:model="filters.number.price.end"]')
-    //         .type('25').should('have.value', '25');
-    //
-    //     cy.get('.power-grid-table tbody tr').should('not.contain.text', '26');
-    // });
-    //
-    // it('filter between 15 - 30', () => {
-    //     cy.get('[wire\\:model="filters.number.price.start"]')
-    //         .type('15').should('have.value', '15');
-    //
-    //     cy.get('[wire\\:model="filters.number.price.end"]')
-    //         .type('30').should('have.value', '30');
-    //
-    //     cy.get('.power-grid-table tbody tr').should('not.contain.text', '31');
-    // });
-    //
-    // it('filter between 5 - 18', () => {
-    //     cy.get('[wire\\:model="filters.number.price.start"]').clear().type('5').should('have.value', '5');
-    //
-    //     cy.get('[wire\\:model="filters.number.price.end"]')
-    //         .type('18').should('have.value', '18');
-    //
-    //     cy.get('.power-grid-table tbody tr').should('not.contain.text', '19');
-    // });
+    /**
+     * ['name' => 'Spicy Tofu Stir Fry', 'calories' => 130, 'created_at' => now()],
+     * ['name' => 'Quinoa Salad with Avocado', 'calories' => 230, 'created_at' => now()],
+     * ['name' => 'Mango Chicken Curry', 'calories' => 330, 'created_at' => now()],
+     * ['name' => 'Grilled Salmon with Lemon Dill Sauce', 'calories' => 430, 'created_at' => now()],
+     * ['name' => 'Vegetarian Buddha Bowl', 'calories' => 530, 'created_at' => now()],
+     * ['name' => 'Pasta Primavera', 'calories' => 630, 'created_at' => now()],
+     * ['name' => 'Blueberry Almond Smoothie Bowl', 'calories' => 730, 'created_at' => now()],
+     * ['name' => 'Grilled Vegetable Wrap', 'calories' => 830, 'created_at' => now()],
+     * ['name' => 'Chocolate Avocado Mousse', 'calories' => 930, 'created_at' => now()],
+     * ['name' => 'Caprese Salad', 'calories' => 1030, 'created_at' => now()],
+     *
+     * @type {string}
+     */
 
+    const tableRows = '.power-grid-table tbody tr';
+    const startInput = '[wire\\:model="filters.number.calories.start"]';
+    const endInput = '[wire\\:model="filters.number.calories.end"]';
+
+    it('filter calories start -> min 400', () => {
+        cy.get(startInput).type('400');
+        cy.get(endInput).clear();
+
+        cy.wait(800) //debounce
+
+        cy.get(tableRows).should('not.contains.text', 'Spicy Tofu Stir Fry')
+        cy.get(tableRows).should('not.contains.text', 'Quinoa Salad with Avocado')
+        cy.get(tableRows).should('not.contains.text', 'Mango Chicken Curry')
+
+        cy.get(tableRows).should('contains.text', 'Vegetarian Buddha Bowl')
+        cy.get(tableRows).should('contains.text', 'Caprese Salad')
+    });
+
+    it('filter calories end -> max 400', () => {
+        cy.get(startInput).clear();
+        cy.get(endInput).type('400');
+
+        cy.wait(800) //debounce
+
+        cy.get(tableRows).should('contains.text', 'Spicy Tofu Stir Fry')
+        cy.get(tableRows).should('contains.text', 'Quinoa Salad with Avocado')
+        cy.get(tableRows).should('contains.text', 'Mango Chicken Curry')
+
+        cy.get(tableRows).should('not.contains.text', 'Grilled Salmon with Lemon Dill Sauce')
+        cy.get(tableRows).should('not.contains.text', 'Caprese Salad')
+    });
+
+    it('filter calories calories between 200 - 400', () => {
+        cy.get(startInput).type('200');
+        cy.get(endInput).type('400');
+
+        cy.wait(800) //debounce
+
+        cy.get(tableRows).should('contains.text', 'Quinoa Salad with Avocado')
+        cy.get(tableRows).should('contains.text', 'Mango Chicken Curry')
+
+        cy.get(tableRows).should('not.contains.text', 'Grilled Salmon with Lemon Dill Sauce')
+        cy.get(tableRows).should('not.contains.text', 'Caprese Salad')
+    });
 })
