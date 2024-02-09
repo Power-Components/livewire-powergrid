3e8ae291810a9c646251690a94e055c7a8a863b1
Dan <daniel@sysanalyst.eu>
2024-02-09 03:58:00 +0100

    Refactor List Database Table

---
+++ src/Actions/ListDatabaseTables.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace PowerComponents\LivewirePowerGrid\Actions;
+
+use Illuminate\Support\Facades\Schema;
+
+final class ListDatabaseTables
+{
+    private const HIDDEN_TABLES = ['failed_jobs', 'migrations', 'password_reset_tokens', 'personal_access_tokens'];
+
+    /**
+     * List tables in database
+     *
+     */
+    public static function handle(): array
+    {
+        return collect(Schema::getTables())
+            ->pluck('name')
+            ->diff(self::HIDDEN_TABLES)->toArray();
+    }
+}
