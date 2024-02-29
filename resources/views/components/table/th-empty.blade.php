997fa6b47a34c113a8a04ecfa609dab1ec2eb1b5
Chase Terry <chase.terry87@gmail.com>
2024-02-29 17:16:35 -0600

    [FIX] Fixes issue with empty th appearing when no data in table. (#1428) (#1436)

--- resources/views/components/table/th-empty.blade.php
+++ resources/views/components/table/th-empty.blade.php
@@ -1,14 +1,12 @@
-<th>
-    <tr
-        class="{{ data_get($theme, 'table.trBodyClass') }}"
-        style="{{ data_get($theme, 'table.trBodyStyle') }}"
+<tr
+    class="{{ data_get($theme, 'table.trBodyClass') }}"
+    style="{{ data_get($theme, 'table.trBodyStyle') }}"
+>
+    <th
+        class="{{ data_get($theme, 'table.tdBodyEmptyClass') }}"
+        style="{{ data_get($theme, 'table.tdBodyEmptyStyle') }}"
+        colspan="{{ ($checkbox ? 1 : 0) + count($columns) + (data_get($setUp, 'detail.showCollapseIcon') ? 1 : 0) }}"
     >
-        <td
-            class="{{ data_get($theme, 'table.tdBodyEmptyClass') }}"
-            style="{{ data_get($theme, 'table.tdBodyEmptyStyle') }}"
-            colspan="{{ ($checkbox ? 1 : 0) + count($columns) + (data_get($setUp, 'detail.showCollapseIcon') ? 1 : 0) }}"
-        >
-            <span>{{ trans('livewire-powergrid::datatable.labels.no_data') }}</span>
-        </td>
-    </tr>
-</th>
+        <span>{{ trans('livewire-powergrid::datatable.labels.no_data') }}</span>
+    </th>
+</tr>
