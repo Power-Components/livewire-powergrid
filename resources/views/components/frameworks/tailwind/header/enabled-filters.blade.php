c5f00331f23a8564c446983c105e2ef542df20ae
Luan Freitas <33601626+luanfreitasdev@users.noreply.github.com>
2024-04-20 12:01:29 -0300

    [Fix] enabled filters tailwind style (#1516)
    * fix enabled filters tailwind style
    
    * build assets

--- resources/views/components/frameworks/tailwind/header/enabled-filters.blade.php
+++ resources/views/components/frameworks/tailwind/header/enabled-filters.blade.php
@@ -1,35 +1,37 @@
 @if (count($enabledFilters))
-    <div data-cy="enabled-filters" class="pg-enabled-filters-base">
+    <div
+        data-cy="enabled-filters"
+        class="pg-enabled-filters-base"
+    >
         @if (count($enabledFilters) > 1)
-            <span class="group pg-enabled-filters-span-clear-all text-pg-primary-100 bg-pg-primary-500 dark:bg-pg-primary-700">
-                {{ trans('livewire-powergrid::datatable.buttons.clear_all_filters') }}
-                <div class="relative flex items-center w-2 h-2">
-                    <button
-                        wire:click.prevent="clearAllFilters"
-                        type="button"
-                    >
-                        <x-livewire-powergrid::icons.x class="w-4 h-4" />
-                    </button>
-                </div>
-            </span>
+            <div class="flex group items-center gap-3 cursor-pointer">
+                <span
+                    wire:click.prevent="clearAllFilters"
+                    class="rounded-full group-hover:text-pg-primary-800 group-hover:dark:text-pg-primary-300 outline-none inline-flex items-center border px-2 py-0.5 font-bold
+                     text-xs border-pg-primary-500 bg-pg-primary-200 dark:bg-pg-primary-600 dark:border-transparent dark:text-pg-primary-100 pg-enabled-filters-span-clear-all"
+                >
+                    {{ trans('livewire-powergrid::datatable.buttons.clear_all_filters') }}
+                    <x-livewire-powergrid::icons.x class="w-4 h-4" />
+                </span>
+            </div>
         @endif
+
         @foreach ($enabledFilters as $filter)
             @isset($filter['label'])
-                <span
+                <div
                     wire:key="enabled-filters-{{ $filter['field'] }}"
-                    class="group pg-enabled-filters-span text-pg-primary-600 dark:text-pg-primary-200 bg-pg-primary-100 dark:bg-pg-primary-600"
+                    class="flex group items-center gap-3 cursor-pointer"
                 >
-                    {{ $filter['label'] }}
-                    <div class="relative flex items-center w-2 h-2">
-                        <button
-                            data-cy="enabled-filters-clear-{{ $filter['field'] }}"
-                            wire:click.prevent="clearFilter('{{ $filter['field'] }}')"
-                            type="button"
-                        >
-                            <x-livewire-powergrid::icons.x class="w-4 h-4" />
-                        </button>
-                    </div>
-                </span>
+                    <span
+                        data-cy="enabled-filters-clear-{{ $filter['field'] }}"
+                        wire:click.prevent="clearFilter('{{ $filter['field'] }}')"
+                        class="rounded-full group-hover:text-pg-primary-600 group-hover:dark:text-pg-primary-300 outline-none inline-flex items-center border px-2 py-0.5 font-bold
+                        text-xs border-pg-primary-500 bg-pg-primary-100 dark:bg-pg-primary-700 dark:bg-opacity-80 dark:border-transparent dark:text-pg-primary-200 dark:text-pg-primary-50 pg-enabled-filters-span"
+                    >
+                        {{ $filter['label'] }}
+                        <x-livewire-powergrid::icons.x class="w-4 h-4" />
+                    </span>
+                </div>
             @endisset
         @endforeach
     </div>
