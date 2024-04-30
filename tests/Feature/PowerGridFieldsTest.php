87ff5a98340449f10d2e4eec102cdea0146b3a0b
Dan <daniel@sysanalyst.eu>
2024-04-30 21:45:10 +0200

    improve pg fields test

--- tests/Feature/PowerGridFieldsTest.php
+++ tests/Feature/PowerGridFieldsTest.php
@@ -1,45 +1,20 @@
 <?php
-use Illuminate\Database\Eloquent\Builder;
-use PowerComponents\LivewirePowerGrid\PowerGrid;
+
+use PowerComponents\LivewirePowerGrid\Tests\Concerns\Components\OrderTable;
 use PowerComponents\LivewirePowerGrid\Tests\Concerns\Models\Order;
 
 use function PowerComponents\LivewirePowerGrid\Tests\Plugins\livewire;
 
-use PowerComponents\LivewirePowerGrid\{Column, PowerGridComponent, PowerGridFields};
+it('can fields with casting and custom fields', function (string $component, object $params) {
+    livewire($component)
+        ->call($params->theme)
+        ->assertSeeHtmlInOrder(['Order 1', 'Order 2', 'Order 3'])
+        ->assertSeeHtmlInOrder(['active', 'active', 'inactive'])
+        ->assertSeeHtmlInOrder(['1000', '2000', '0'])
+        ->assertSeeHtmlInOrder(['127.3', '259.5', '']);
+})->with('order_table');
 
-$component = new class () extends PowerGridComponent {
-    public function datasource(): Builder
-    {
-        return Order::query();
-    }
-
-    public function fields(): PowerGridFields
-    {
-        return PowerGrid::fields()
-            ->add('name')
-            ->add('tax')
-            ->add('price')
-            ->add('price_formatted', fn (Order $model) => $model->price * 100);
-    }
-
-    public function columns(): array
-    {
-        return [
-            Column::make('Name', 'name'),
-            Column::make('Price', 'price_formatted', 'price'),
-            Column::make('Tax', 'tax'),
-        ];
-    }
-};
-
-it('can add fields', function (string $name, string|float $price, string|float $tax) use ($component) {
-    $component = livewire($component::class);
-
-    $component->assertSee($name)
-              ->assertSee($price)
-              ->assertSee($tax);
-})->with([
-    ['Order 1', 1000, 127.30],
-    ['Order 2', 2000, 259.50],
-    ['Order 3', '', ''],
+dataset('order_table', [
+    'tailwind'  => [OrderTable::class, (object) ['theme' => 'tailwind']],
+    'bootstrap' => [OrderTable::class, (object) ['theme' => 'bootstrap']],
 ]);
