77ccb7c20dfb5e637d9c09e2f43d649e442d92c9
Rohit Shakya <92146757+rohitshakyaa@users.noreply.github.com>
2024-05-05 00:55:25 +0545

    loading ui fix for bootstrap (#1523)

--- resources/views/components/frameworks/bootstrap5/header/loading.blade.php
+++ resources/views/components/frameworks/bootstrap5/header/loading.blade.php
@@ -3,7 +3,7 @@
     style="padding-left: 10px;"
 >
     <div
-        wire:loading.class="spinner-border d-none"
+        wire:loading.class="spinner-border"
         role="status"
         style="color: #656363;"
     >
