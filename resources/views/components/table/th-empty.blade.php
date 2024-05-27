f981fda75b8d226efb7c99f9eea7c3734943d89c
luanfreitasdev <luanfreitas10@protonmail.com>
2024-05-27 13:36:34 -0300

    remove unnecessary count colspan

--- resources/views/components/table/th-empty.blade.php
+++ resources/views/components/table/th-empty.blade.php
@@ -5,7 +5,7 @@
     <th
         class="{{ data_get($theme, 'table.tdBodyEmptyClass') }}"
         style="{{ data_get($theme, 'table.tdBodyEmptyStyle') }}"
-        colspan="{{ ($checkbox ? 1 : 0) + count($columns) + (data_get($setUp, 'detail.showCollapseIcon') ? 1 : 0) }}"
+        colspan="999"
     >
             {!! $this->processNoDataLabel() !!}
     </th>
