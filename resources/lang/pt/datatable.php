12b780fb12173eecae61005f34fbce05ecc8ebe2
helderneves <helderneves@orbiapps.pt>
2024-09-05 10:52:15 +0100

    Updated PT-PT translations

---
+++ resources/lang/pt/datatable.php
@@ -0,0 +1,75 @@
+<?php
+
+return [
+    'buttons' => [
+        'filter'            => 'Filtrar',
+        'clear_all_filters' => 'Limpar Todos',
+    ],
+    'labels' => [
+        'action'           => 'Ações',
+        'results_per_page' => 'Registos por página',
+        'clear_filter'     => 'Limpar filtro',
+        'no_data'          => 'Nenhum registo encontrado',
+        'all'              => 'Todos',
+        'selected'         => 'Selecionados',
+        'filtered'         => 'Filtrados',
+    ],
+    'placeholders' => [
+        'search' => 'Pesquisar ...',
+        'select' => 'Selecione um período',
+    ],
+    'pagination' => [
+        'showing' => 'A mostrar',
+        'to'      => 'até',
+        'of'      => 'de',
+        'results' => 'resultados',
+        'all'     => 'Todos',
+    ],
+    'multi_select' => [
+        'select' => 'Selecione',
+        'all'    => 'Todos',
+    ],
+    'select' => [
+        'select' => 'Selecione',
+        'all'    => 'Todos',
+    ],
+    'boolean_filter' => [
+        'all' => 'Todos',
+    ],
+    'input_text_options' => [
+        'is'           => 'É exatamente',
+        'is_not'       => 'É diferente de',
+        'contains'     => 'Contém',
+        'contains_not' => 'Não contém',
+        'starts_with'  => 'Começa com',
+        'ends_with'    => 'Termina com',
+        'is_null'      => 'É nulo',
+        'is_not_null'  => 'Não é núlo',
+        'is_blank'     => 'Está em branco',
+        'is_not_blank' => 'Não está em branco',
+        'is_empty'     => 'Não está preenchido',
+        'is_not_empty' => 'Está preenchido',
+    ],
+    'export' => [
+        'exporting' => 'Por favor, aguarde!',
+        'completed' => 'Exportação concluída! Os seus ficheiros estão prontos para download',
+    ],
+    'soft_deletes' => [
+        'message_with_trashed' => 'A mostrar todos os registos, incluindo os excluídos.',
+        'message_only_trashed' => 'A mostrar apenas os registos excluídos.',
+        'without_trashed'      => 'Sem excluídos',
+        'with_trashed'         => 'Com excluídos',
+        'only_trashed'         => 'Apenas excluídos',
+    ],
+    'multi_sort' => [
+        'message' => 'A classificação múltipla está ativa',
+    ],
+    'buttons_macros' => [
+        'confirm' => [
+            'message' => 'Tem a certeza que pretende executar esta ação?',
+        ],
+        'confirm_prompt' => [
+            'message' => "Tem a certeza que pretende executar esta ação? \n\n Escreva :confirmValue para confirmar.",
+        ],
+    ],
+];
