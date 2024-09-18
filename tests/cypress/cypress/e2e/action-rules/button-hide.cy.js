829677c3ed8714c11ac803e5ad8d104775aafe7d
luanfreitasdev <luanfreitas10@protonmail.com>
2024-09-18 11:20:50 -0300

    Fix cypress test

--- tests/cypress/cypress/e2e/action-rules/button-hide.cy.js
+++ tests/cypress/cypress/e2e/action-rules/button-hide.cy.js
@@ -5,12 +5,12 @@ describe('render action rules buttons hide', () => {
 
     it('can render buttons hide', () => {
         cy.get('.power-grid-table tbody tr').eq(0)
-            .should('contain.text', 'Edit-1')
-            .should('not.contain.text', 'Delete-1')
+            .should('not.contain.text', 'Edit-1')
+            .should('contain.text', 'Delete-1')
 
         cy.get('.power-grid-table tbody tr').eq(1)
-            .should('contain.text', 'Delete-2')
-            .should('not.contain.text', 'Edit-2')
+            .should('not.contain.text', 'Delete-2')
+            .should('contain.text', 'Edit-2')
 
         cy.get('.power-grid-table tbody tr').eq(2)
             .should('contain.text', 'Edit-3')
