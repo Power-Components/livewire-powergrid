85272d88ec5604caf6dd04830b804c85dd6d6217
luanfreitasdev <luanfreitas10@protonmail.com>
2024-12-12 17:28:14 -0300

    replace laravel-mix to vite

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+/** @type {import('vite').UserConfig} */
+export default {
+    build: {
+        assetsDir: '',
+        manifest: true,
+        rollupOptions: {
+            input: [
+                'resources/js/powergrid.js',
+                'resources/css/tailwind.css',
+                'resources/css/bootstrap5.css'
+            ],
+            output: {
+                entryFileNames: '[name].js',
+                chunkFileNames: '[name].js',
+                assetFileNames: '[name].[ext]',
+            },
+        },
+    },
+};
