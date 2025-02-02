f3236196b6b616f5496f62d0d63a6bbfd0fdbb21
Tiago <tiagospem@gmail.com>
2025-02-02 17:08:29 -0300

    added missing imports and throws

--- src/Commands/Actions/ListModels.php
+++ src/Commands/Actions/ListModels.php
@@ -3,19 +3,20 @@
 namespace PowerComponents\LivewirePowerGrid\Commands\Actions;
 
 use Illuminate\Support\Facades\File;
+use ReflectionClass;
+use ReflectionException;
 use Symfony\Component\Finder\SplFileInfo;
 
 final class ListModels
 {
     /**
      * List files in Models
-     *
      */
     public static function handle(): array
     {
         $directories = config('livewire-powergrid.auto_discover_models_paths', [app_path('Models')]);
 
-        /** @var Array<int,string> $directories */
+        /** @var array<int,string> $directories */
         return collect($directories)
             ->filter(fn (string $directory) => File::exists($directory))
             ->map(fn (string $directory) => File::allFiles($directory))
@@ -33,8 +34,10 @@ final class ListModels
             ->filter()
 
             // Remove classes that do not extend an Eloquent Model
-            /** @phpstan-ignore-next-line */
-            ->reject(fn (string $fqnClass) => rescue(fn () => (new \ReflectionClass($fqnClass))->isSubclassOf(\Illuminate\Database\Eloquent\Model::class), false) === false)
-            ->toArray();
+            /** @phpstan-ignore-next-line
+             * @throws ReflectionException
+             */
+            ->reject(fn (string $fqnClass) => rescue(fn () => (new ReflectionClass($fqnClass))->isSubclassOf(\Illuminate\Database\Eloquent\Model::class), false) === false)
+            ->all();
     }
 }
