ba4fd765ff45832d52f83aba3bdc339f6e140da2
Matthieu Lempereur <matt.lempereur@gmail.com>
2025-03-07 10:18:52 +0100

    fix

--- src/Button.php
+++ src/Button.php
@@ -23,7 +23,7 @@ use Livewire\Wireable;
  * @method static confirm(string $message = 'Are you sure you want to perform this action?')
  * @method static confirmPrompt(string $message = 'Are you sure you want to perform this action?', string $confirmValue = 'Confirm')
  * @method static class(string $classes)
- * @method static disable(bool = true)
+ * @method static disable(bool $disable = true)
  */
 final class Button implements Wireable
 {
