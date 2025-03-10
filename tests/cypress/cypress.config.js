a4b4a824ce892426abd389e9ecff030ca2ca6453
luanfreitasdev <luanfreitas10@protonmail.com>
2025-03-10 12:11:07 -0300

    fix cypress config

--- tests/cypress/cypress.config.js
+++ tests/cypress/cypress.config.js
@@ -1,6 +1,6 @@
-const { defineConfig } = require("cypress");
+import { defineConfig } from "cypress";
 
-module.exports = defineConfig({
+export default defineConfig({
     env: {
         type: "local",
     },
