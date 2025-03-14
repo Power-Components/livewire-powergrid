98206285f7f1401897f50009e0fd10ca1527f7bf
luanfreitasdev <luanfreitas10@protonmail.com>
2025-03-14 09:43:20 -0300

    Remove ds

--- src/Traits/ExportableJob.php
+++ src/Traits/ExportableJob.php
@@ -68,16 +68,7 @@ trait ExportableJob
             ->offset($this->offset)
             ->limit($this->limit)
             ->orderBy($property('sortField'), $processDataSource->component->sortDirection)
-            ->ds()
             ->get();
-
-        ds(
-            [
-                'limit'  => $this->limit,
-                'offset' => $this->offset,
-                'data'   => $results,
-            ]
-        );
 
         return DataSourceBase::transform($results, $this->componentTable);
     }
