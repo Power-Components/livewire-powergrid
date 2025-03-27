c610ac427f25ea450cd192b83c9dcc11f2a258f9
4rc4d14 <info@lucaiadicicco.it>
2025-03-27 14:40:17 +0100

    Refactor InputText attributes structure for input and select fields.
    - Simplified the attribute structure for input and select fields in the InputText class.

--- src/FilterAttributes/InputText.php
+++ src/FilterAttributes/InputText.php
@@ -10,14 +10,14 @@ class InputText
     public function __invoke(string $field, string $title): array
     {
         return [
-            'inputAttributes' => new ComponentAttributeBag([
+            'inputAttributes' => [
                 'wire:model' => 'filters.input_text.' . $field,
                 'wire:input.live.debounce.600ms' => "filterInputText('{$field}', \$event.target.value, '{$title}')",
-            ]),
-            'selectAttributes' => new ComponentAttributeBag([
+            ],
+            'selectAttributes' => [
                 'wire:model' => 'filters.input_text_options.' . $field,
                 'wire:input.live.debounce.600ms' => "filterInputTextOptions('{$field}', \$event.target.value, '{$title}')",
-            ]),
+            ],
         ];
     }
 }
