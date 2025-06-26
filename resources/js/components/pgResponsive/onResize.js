924962cb1f4b8c1207da1bbd2af9a3306151d789
denosaurusz <105290804+denosaurusz@users.noreply.github.com>
2025-06-26 16:18:57 +0200

    Update onResize.js
    Remove hidden columns from responsive expanded.

--- resources/js/components/pgResponsive/onResize.js
+++ resources/js/components/pgResponsive/onResize.js
@@ -20,7 +20,11 @@ function getTableUtilWidth(element) {
 function getItemsToHide(element, tableWidth) {
     const items = [].slice.call(element.querySelectorAll('table thead tr:nth-child(1) th'))
 
-    const itemsSorted = [].slice.call(items).sort(function (a,b) {
+    const visibleThs = Array.from(items).filter(th => {
+        return getComputedStyle(th).display !== 'none';
+    });
+
+    const itemsSorted = [].slice.call(visibleThs).sort(function (a,b) {
         const firstSortOrder = a.getAttribute('sort_order') ??  999
         const secondSortOrder = b.getAttribute('sort_order') ??  999
 
@@ -43,7 +47,7 @@ function getItemsToHide(element, tableWidth) {
         if (fitsMoreItems && calc <= tableWidth && (calc + itemWidth <= tableWidth)) {
             calc += itemWidth;
         } else {
-            itemsToHide.push(items.indexOf(item) + 1)
+            itemsToHide.push(visibleThs.indexOf(item) + 1)
             fitsMoreItems = false
         }
     })
