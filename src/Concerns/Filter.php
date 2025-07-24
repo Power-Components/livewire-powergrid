01b4291188d227696a40626e0e21d6cc7832a9f5
johannesMEGABAD <johannes.werner@megabad.de>
2025-07-24 08:53:20 +0200

    fix: forgot to use the exception

--- src/Concerns/Filter.php
+++ src/Concerns/Filter.php
@@ -2,6 +2,7 @@
 
 namespace PowerComponents\LivewirePowerGrid\Concerns;
 
+use Carbon\Exceptions\InvalidFormatException;
 use Closure;
 use Exception;
 use Illuminate\Support\{Arr, Carbon, Collection, Str};
@@ -134,11 +135,15 @@ trait Filter
         $appTimezone = config('app.timezone');
         $isDatetime = $type === 'datetime';
         $hasTime = str_contains($dateFormat, 'H');
+        
+        $makeDate = function ($dateStr) use ($hasTime, $appTimezone) {
+            try {
+                $date = Carbon::parse($dateStr, $appTimezone);
+            } catch (InvalidFormatException) {
+                return now($appTimezone);
+            }
 
-        $makeDate = function ($dateStr) use ($dateFormat, $hasTime, $appTimezone) {
-            $date = Carbon::createFromFormat($dateFormat, $dateStr, $appTimezone);
-
-            if (! $hasTime) {
+            if (!$hasTime) {
                 $date->setTime(0, 0, 0);
             }
 
