2719607dc464d5d7ce8606029b0e6760a69ee3d1
Matthieu Lempereur <matt.lempereur@gmail.com>
2025-11-02 16:24:22 +0100

    test: unskip several actions tests (#2029)

--- tests/Feature/Actions/AskComponentNameTest.php
+++ tests/Feature/Actions/AskComponentNameTest.php
@@ -4,6 +4,6 @@ use Laravel\Prompts\{Key, Prompt};
 use PowerComponents\LivewirePowerGrid\Commands\Actions\AskComponentName;
 
 test('input component name', function () {
-    Prompt::fake([...str_split('New'), Key::ENTER]);
+    Prompt::fake(['New', Key::ENTER]);
     expect(AskComponentName::handle())->toBe('UserTableNew');
-})->skip('Check test');
+});
