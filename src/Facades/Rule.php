81aa5134a78b07d0183110ecbef9b0e6139d39a6
Matthieu Lempereur <matt.lempereur@gmail.com>
2026-01-30 15:30:20 +0100

    fix

--- src/Facades/Rule.php
+++ src/Facades/Rule.php
@@ -3,10 +3,10 @@
 namespace PowerComponents\LivewirePowerGrid\Facades;
 
 use Illuminate\Support\Facades\Facade;
-use PowerComponents\LivewirePowerGrid\Components\Rules\{RuleButton, RuleCheckbox, RuleEditOnClick, RuleRadio, RuleRows, RuleToggleable};
+use PowerComponents\LivewirePowerGrid\Components\Rules\{RuleActions, RuleCheckbox, RuleEditOnClick, RuleRadio, RuleRows, RuleToggleable};
 
 /**
- * @method static RuleButton button(string $button)
+ * @method static RuleActions button(string $button)
  * @method static RuleToggleable toggleable(string $column)
  * @method static RuleEditOnClick editOnClick(string $column)
  * @method static RuleRows rows()
