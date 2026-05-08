Power-Components/livewire-powergrid

commit 1621683cace6fd1f4607d3fde0dd5525e516f43e

Update onResize.js (#1940)
Remove hidden columns from responsive expanded.

resources/js/components/pgResponsive/onResize.js
@@ -20,7 +20,11 @@ function getTableUtilWidth(element) {
 function getItemsToHide(element, tableWidth) {
     const items = [].slice.call(element.querySelectorAll('table thead tr:nth-child(1) th'))
 
-    const itemsSorted = [].slice.call(items).sort(function (a,b) {
+    const visibleThs = Array.from(items).filter(th => {
+        return getComputedStyle(th).display !== 'none';
+    });
+
+    const itemsSorted = [].slice.call(visibleThs).sort(function (a,b) {
         const firstSortOrder = a.getAttribute('sort_order') ??  999
         const secondSortOrder = b.getAttribute('sort_order') ??  999
 
@@ -43,7 +47,7 @@ function getItemsToHide(element, tableWidth) {
         if (fitsMoreItems && calc <= tableWidth && (calc + itemWidth <= tableWidth)) {
             calc += itemWidth;
         } else {
-            itemsToHide.push(items.indexOf(item) + 1)
+            itemsToHide.push(visibleThs.indexOf(item) + 1)
             fitsMoreItems = false
         }
     })

commit 2c26e895763fecd9c5a312c223a4c26a299525c0

Fix off-by-one in pgResponsive when any <th> is hidden introduced in #1940

resources/js/components/pgResponsive/onResize.js
@@ -47,7 +47,7 @@ function getItemsToHide(element, tableWidth) {
         if (fitsMoreItems && calc <= tableWidth && (calc + itemWidth <= tableWidth)) {
             calc += itemWidth;
         } else {
-            itemsToHide.push(visibleThs.indexOf(item) + 1)
+            itemsToHide.push(items.indexOf(item) + 1)
             fitsMoreItems = false
         }
     })
@@ -79,7 +79,7 @@ function fillTableExpand(element, hideItems) {
 
             if (!expandContainer) continue
 
-            let rowName = element.querySelector(`table thead tr th:nth-child(${hideItem}) span[data-value]`).textContent
+            let rowName = element.querySelector(`table thead tr th:nth-child(${hideItem}) span[data-value]`).textContent  ?? ''
 
             const rowValue = row.querySelector(`tr:last-child td:nth-child(${hideItem})`)?.innerHTML
 