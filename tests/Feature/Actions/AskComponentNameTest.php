d2045b8c0db0bae4f4b984790390c6f90071e6d7
luanfreitasdev <luanfreitas10@protonmail.com>
2026-09-04 18:53:47 -0300

    chore: remove unit-only tests

--- tests/Feature/Actions/AskComponentNameTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Laravel\Prompts\{Key, Prompt};
-use PowerComponents\LivewirePowerGrid\Commands\Actions\AskComponentName;
-
-test('input component name', function () {
-    Prompt::fake(['New', Key::ENTER]);
-    expect(AskComponentName::handle())->toBe('UserTableNew');
-});
